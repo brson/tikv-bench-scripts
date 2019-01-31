9c76aabb4a2c2bf38ec1d39c3a5ceb19a203259e
Brian Anderson <andersrb@gmail.com>
2019-01-31 09:52:14 +0000

    .

--- bench-cmp.sh
+++ bench-cmp.sh
@@ -1,11 +1,11 @@
 set -e
 set -x
 
-cargo benchcmp tikv-before.txt tikv-after.txt
-cargo benchcmp codec-before.txt codec-after.txt
-cargo benchcmp misc-before.txt misc-after.txt
-cargo benchcmp test_util-before.txt test_util-after.txt
-critcmp raftstore-before raftstore-after
-critcmp coprocessor_executors-before coprocessor_executors-after
-critcmp hierarchy-before hierarchy-after
+cargo benchcmp tikv-1.txt tikv-2.txt
+cargo benchcmp codec-1.txt codec-2.txt
+cargo benchcmp misc-1.txt misc-2.txt
+cargo benchcmp test_util-1.txt test_util-2.txt
+critcmp raftstore-1 raftstore-2
+critcmp coprocessor_executors-1 coprocessor_executors-2
+critcmp hierarchy-1 hierarchy-2
 
--- bench-rec.sh
+++ bench-rec.sh
@@ -29,37 +29,37 @@ before_commit="$1"
 after_commit="$2"
 
 git checkout $before_commit
-cargo bench --lib 2>&1 | tee tikv-before.txt
+cargo bench --lib 2>&1 | tee tikv-1.txt
 git checkout $after_commit
-cargo bench --lib 2>&1 | tee tikv-after.txt
+cargo bench --lib 2>&1 | tee tikv-2.txt
 
 git checkout $before_commit
-cargo bench -p codec 2>&1 | tee codec-before.txt
+cargo bench -p codec 2>&1 | tee codec-1.txt
 git checkout $after_commit
-cargo bench -p codec 2>&1 | tee codec-after.txt
+cargo bench -p codec 2>&1 | tee codec-2.txt
 
 git checkout $before_commit
-cargo bench --bench misc 2>&1 | tee misc-before.txt
+cargo bench --bench misc 2>&1 | tee misc-1.txt
 git checkout $after_commit
-cargo bench --bench misc 2>&1 | tee misc-after.txt
+cargo bench --bench misc 2>&1 | tee misc-2.txt
 
 git checkout $before_commit
-cargo bench -p test_util 2>&1 | tee test_util-before.txt
+cargo bench -p test_util 2>&1 | tee test_util-1.txt
 git checkout $after_commit
-cargo bench -p test_util 2>&1 | tee test_util-after.txt
+cargo bench -p test_util 2>&1 | tee test_util-2.txt
 
 git checkout $before_commit
-cargo bench --bench raftstore -- --save-baseline raftstore-before
+cargo bench --bench raftstore -- --save-baseline raftstore-1
 git checkout $after_commit
-cargo bench --bench raftstore -- --save-baseline raftstore-after
+cargo bench --bench raftstore -- --save-baseline raftstore-2
 
 git checkout $before_commit
-cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-before
+cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-1
 git checkout $after_commit
-cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-after
+cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-2
 
 git checkout $before_commit
-cargo bench --bench hierarchy -- --save-baseline hierarchy-before
+cargo bench --bench hierarchy -- --save-baseline hierarchy-1
 git checkout $after_commit
-cargo bench --bench hierarchy -- --save-baseline hierarchy-after
+cargo bench --bench hierarchy -- --save-baseline hierarchy-2
 
