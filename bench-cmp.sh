cc1c6fd69ee0251c8b751a932a90f27029e9a03e
Brian Anderson <andersrb@gmail.com>
2019-01-31 05:51:32 +0000

    Various fixes

--- bench-cmp.sh
+++ bench-cmp.sh
@@ -1,9 +1,11 @@
 set -e
 set -x
 
-cargo benchcmp test_util-before.txt test_util-after.txt
 cargo benchcmp tikv-before.txt tikv-after.txt
+cargo benchcmp codec-before.txt codec-after.txt
 cargo benchcmp misc-before.txt misc-after.txt
+cargo benchcmp test_util-before.txt test_util-after.txt
 critcmp raftstore-before raftstore-after
 critcmp coprocessor_executors-before coprocessor_executors-after
 critcmp hierarchy-before hierarchy-after
+
--- bench-rec.sh
+++ bench-rec.sh
@@ -1,26 +1,65 @@
-# Run and store TiKV benchmarks in a format that can be used by cargo-benchcmp and critcmp
+# Runs and store TiKV benchmarks in a format that can be used by
+# [cargo-benchcmp] and [critcmp].
+#
+# [cargo-benchcmp]: https://github.com/BurntSushi/cargo-benchcmp
+# [critcmp]: https://github.com/BurntSushi/critcmp
+#
+# From your cargo project directory:
+#
+# bench-cmp.sh $before-commit $after-commit
+#
+# It will modify your git workspace, so make sure it's committed and
+# clean.
+#
+# For #[bench] benchmarks, this script will save the output
+# in .txt files in the current directory. For criterion benchmarks,
+# it will save "baselines" in the `target` directory.
+#
+# Run bench-cmp.sh afterward to get a comparison report.
 
 set -e
 set -x
 
-before_commit=1f43235e09045d3d31bcc9730ce017204e717663
-after_commit=72bc513d
-
-# TODO this '--lib tikv' invocation doesn't work
+if [[ $1 == "" || $2 == "" ]]; then
+	echo "usage: bench-rec.sh $before-commit $after-commit"
+	exit 1
+fi
+	
+before_commit="$1"
+after_commit="$2"
 
 git checkout $before_commit
-cargo bench -p test_util 2>&1 | tee -a test_util-before.txt
-cargo bench --lib 2>&1 | tee -a tikv-before.txt
-cargo bench --bench misc 2>&1 | tee -a misc-before.txt
-cargo bench --bench raftstore -- --save-baseline raftstore-before
-cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-before
-cargo bench --bench hierarchy -- --save-baseline hierarchy-before
-
+cargo bench --lib 2>&1 | tee tikv-before.txt
 git checkout $after_commit
-cargo bench -p test_util 2>&1 | tee -a test_util-after.txt
-cargo bench --lib 2>&1 | tee -a tikv-after.txt
-cargo bench --bench misc 2>&1 | tee -a misc-after.txt
+cargo bench --lib 2>&1 | tee tikv-after.txt
+
+git checkout $before_commit
+cargo bench -p codec 2>&1 | tee codec-before.txt
+git checkout $after_commit
+cargo bench -p codec 2>&1 | tee codec-after.txt
+
+git checkout $before_commit
+cargo bench --bench misc 2>&1 | tee misc-before.txt
+git checkout $after_commit
+cargo bench --bench misc 2>&1 | tee misc-after.txt
+
+git checkout $before_commit
+cargo bench -p test_util 2>&1 | tee test_util-before.txt
+git checkout $after_commit
+cargo bench -p test_util 2>&1 | tee test_util-after.txt
+
+git checkout $before_commit
+cargo bench --bench raftstore -- --save-baseline raftstore-before
+git checkout $after_commit
 cargo bench --bench raftstore -- --save-baseline raftstore-after
+
+git checkout $before_commit
+cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-before
+git checkout $after_commit
 cargo bench --bench coprocessor_executors -- --save-baseline coprocessor_executors-after
+
+git checkout $before_commit
+cargo bench --bench hierarchy -- --save-baseline hierarchy-before
+git checkout $after_commit
 cargo bench --bench hierarchy -- --save-baseline hierarchy-after
 
